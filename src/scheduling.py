30c13c634e5dbf543ceeb76a1a19df452976146b
marija.piliskic <marija.piliskic@gideonbros.ai>
2023-01-18 14:41:37 +0100

    karajfilip - functions for report

--- src/scheduling.py
+++ src/scheduling.py
@@ -2,6 +2,32 @@ from numpy import inf
 from JobShop import *
 import networkx as nx
 
+def calculate_CTi(out, time):
+    CTi = []
+    for i, j in zip(out, time):
+        CTi.append(i + j)
+    return CTi
+
+def calculate_CT(CTi):
+    return sum(CTi)/len(CTi)
+
+def calculate_CTku(CTi, deadline):
+    CTku = []
+    for i, j in zip(CTi, deadline):
+        CTku.append(i - j)
+    return CTku
+
+def calculate_CTu(CTku):
+    m = max(CTku)
+    return max(m, 0)
+
+def calculate_TH(CTi):
+    M = max(CTi)
+    return len(CTi)/M
+
+def calculate_WIP(CT, TH):
+    return  CT * TH
+
 def get_start_times(t: list, order: list) -> list:
     out = [-1 for i in range(len(t))]
     current_t = 0
@@ -85,7 +111,20 @@ if 'a' in task.lower():
     elif mode.upper() == 'MOORE':
         out = Moore(time,deadline)
 
-    print(out)
+    CTi = calculate_CTi(out, time)
+    CT = calculate_CT(CTi)
+    CTku = calculate_CTku(CTi, deadline)
+    CTu = calculate_CTu(CTku)
+    TH = calculate_TH(CTi)
+    WIP = calculate_WIP(CT, TH)
+
+    print(f's: {out}')
+    print(f'CTi: {CTi}')
+    print(f'CT: {CT}')
+    print(f'CTku: {CTku}')
+    print(f'CTu: {CTu}')
+    print(f'TH: {TH}')
+    print(f'WIP: {WIP}')
 
 elif 'b' in task.lower():
     J = eval(input('Unesite definiciju sustava u obliku liste listi uredenih parova: '))
